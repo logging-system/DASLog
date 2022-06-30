800d38e1cf74525fe7fdec1b325be8f6684a5ad6
logging-system <97616562+logging-system@users.noreply.github.com>
2022-06-30 08:48:31 +0200

    Update TXcount.sh

--- DASLog-Decentralized Auditable Secure Logging for UAV/LS/TXcount.sh
+++ DASLog-Decentralized Auditable Secure Logging for UAV/LS/TXcount.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-#s=$(($(date +%s%N)/1000000))
 node writeRootAdd2RDS.js
-
-#e=$(($(date +%s%N)/1000000))
-#echo "Completed in"
-#echo  $(($e - $s))
-#echo "milliseconds"
 echo "Root inf. is stored to the RDS ..."
 echo "--------------------------------------------------------"
