4f10c39ee0263788799f2712446e2de91c2cdfe9
logging-system <97616562+logging-system@users.noreply.github.com>
2023-03-26 10:33:39 +0200

    Update system-bench.py

--- DASLog-Decentralized Auditable Secure Logging for UAV/Benchmark/system-bench.py
+++ DASLog-Decentralized Auditable Secure Logging for UAV/Benchmark/system-bench.py
@@ -9,8 +9,21 @@ from cryptography.hazmat.primitives import hashes, hmac
 from cryptography.hazmat.backends import default_backend
 import base64
 import os
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from log_sig_verify import verify_signature
 
+# Generate RSA key pair
+private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
+public_key = private_key.public_key()
 
+# Sign log with private key
+def sign_log(log_message):
+    signature = private_key.sign(log_message,
+                                 padding.PSS(mgf=padding.MGF1(hashes.SHA256()), salt_length=padding.PSS.MAX_LENGTH),
+                                 hashes.SHA256())
+    return signature
+
+# hashing function
 def hashing_function(x):
     m = hashlib.sha256()
     m.update(x.encode('utf8'))
@@ -25,6 +38,7 @@ a = []
 a1 = []
 b = []
 bhc = []
+bsig = []
 c = []
 sec = 50236095557359180159959298694639776544818204649813293341147137887852349615765
 salt = b'\t\xee2\xf7\xc4Jz\x1ez.\x1e6\x08K\n\xef'  # fixed
@@ -36,6 +50,12 @@ kdf = PBKDF2HMAC(
     backend=default_backend(),
     iterations=100000)
 key = base64.urlsafe_b64encode(kdf.derive(str(sec).encode()))
+
+# Sign the log_data
+log_data = str(logs())
+sl = sign_log(log_data.encode("utf-8"))
+#print('sl:', sl)
+
 for j in range(1, t+1):
 
     n = j * nl # number of leaves
@@ -43,6 +63,11 @@ for j in range(1, t+1):
     for i in range(0, n):
         ldata = logs()
         Loglist.append(str(ldata))
+
+    # Verify the signature
+    tic_sig = time.process_time()
+    verify_signature(public_key, log_data.encode("utf-8"), sl)
+    toc_sig = time.process_time()
 
     tic = time.process_time()
     # hash_chain
@@ -54,9 +79,9 @@ for j in range(1, t+1):
         f_key = Fernet(key)
         e_data = f_key.encrypt(record.encode('utf-8'))
         iEncData = e_data.decode("utf-8")
-
-    # Populate tree with records
-    tic1 = time.process_time()
+    toc_DASLod = time.process_time()
+    # Populate tree with records - Simple-DASLog
+    #tic1 = time.process_time() #only MT
     tree = MerkleTree()
     for record in Loglist:
         tree.encrypt(record)
@@ -67,8 +92,9 @@ for j in range(1, t+1):
     js.append(str(fs * 0.001))  # KB
     toc = time.process_time()
     tr.append(str(ts*0.032))# KB
-    a1.append(str(toc - tic1))
-    a.append(str(toc - tic))
+    #a1.append(str(toc - tic1)) #only MT
+    a1.append(str(toc_DASLod - tic))  # DASLog
+    a.append(str(toc - tic))  # Simple-DASLog
     c.append(str(n))
     # Prove and verify
     tic2 = time.process_time()
@@ -81,6 +107,7 @@ for j in range(1, t+1):
         print('hashchain is not verified')
 
     toc02 = time.process_time()
+    tic2_sig = time.process_time()
     loaded_tree = MerkleTree.fromJSONFile('log_path.json')
 
     state = loaded_tree.get_root_hash()
@@ -94,6 +121,7 @@ for j in range(1, t+1):
     print(proof.verify())
     bhc.append(str(toc02 - tic2))
     b.append(str(toc2 - tic2))
+    bsig.append(str((toc_sig - tic_sig) + (toc2 - tic2_sig)))
 
 data = pd.DataFrame(
     {'C': c, 'A': a}
@@ -130,3 +158,9 @@ data = pd.DataFrame(
     )
 with pd.ExcelWriter('tree_sheets_bhc.xlsx') as writer1:
     data.to_excel(writer1, sheet_name='df_5')
+
+data = pd.DataFrame(
+    {'C': c, 'Bsig': bsig}
+    )
+with pd.ExcelWriter('tree_sheets_bsig.xlsx') as writer1:
+    data.to_excel(writer1, sheet_name='df_6')
